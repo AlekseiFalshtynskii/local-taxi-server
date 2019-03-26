8b71821dde44cc25b9bdc134a7f249692c5c177b
AlekseiFalshtynskii <aleksei.falshtynskii@gmail.com>
2019-03-26 23:13:07 +0300

    create

---
+++ src/main/java/ru/spring/localtaxi/LocalTaxiApplication.java
@@ -0,0 +1,13 @@
+package ru.spring.localtaxi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LocalTaxiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LocalTaxiApplication.class, args);
+	}
+
+}
