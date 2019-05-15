7309510916e7c239c54d5cb5f9adff2cc57d7fe7
AlekseiFalshtynskii <aleksei.falshtynskii@gmail.com>
2019-05-15 17:46:23 +0300

    auth-service feign config

--- auth-service/auth-service-api/src/main/java/ru/spring/localtaxi/authserviceapi/client/UserClient.java
+++ auth-service/auth-service-api/src/main/java/ru/spring/localtaxi/authserviceapi/client/UserClient.java
@@ -2,8 +2,9 @@ package ru.spring.localtaxi.authserviceapi.client;
 
 import org.springframework.cloud.openfeign.FeignClient;
 import ru.spring.localtaxi.authserviceapi.api.UserApi;
+import ru.spring.localtaxi.authserviceapi.config.FeignConfiguration;
 
-@FeignClient("auth-service")
+@FeignClient(name = "auth-service", configuration = FeignConfiguration.class)
 public interface UserClient extends UserApi {
 
 }
